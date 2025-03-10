1e37c7943e3ee4fabd0883f3a643502f3192c107
HABEEBA SINU <131899687+Habeebasinu@users.noreply.github.com>
2025-03-10 17:35:10 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,18 @@
+function myfunction(str){
+    console.log(str.toUpperCase())
+}
+myfunction("hello world")
+
+str=" hello  world "
+console.log(str.length)
+console.log(str.includes("hello"));
+console.log(str.trim());
+console.log(str.replace("world","World"))
+console.log(str.charAt(1));
+console.log(str.charAt(1).toUpperCase()+str.slice(2));
+console.log(str+="!!!");
+
+
+
+
+
