613ef17825e98dc5d1dceb16b6b7df3fa47137ad
HABEEBA SINU <131899687+Habeebasinu@users.noreply.github.com>
2025-03-18 14:50:24 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,11 @@
+const words=["hello","google"]
+words.push("!!!")
+console.log(words)
+console.log(words.join(" "))
+
+const myfunction=(str)=>{
+    console.log(str.split('').reverse().join(''))
+
+    
+}
+myfunction("hello")
